4b3f32b329c4a4fd486d961000ce35e378ef2486
Mesut Suhan Şişman <127686800+Mesutssmn@users.noreply.github.com>
2025-02-23 13:23:55 +0300

    Update app.py

--- app.py
+++ app.py
@@ -185,7 +185,7 @@ try:
         rtc_configuration={
             "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
         },
-        async_processing=True
+        async_processing=False
     )
 except Exception as e:
     logging.error(f"Error in WebRTC Streamer: {e}")
